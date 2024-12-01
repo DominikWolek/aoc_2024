1c9cdd0e0157d08b901af7ffff4e25bf1ad486d1
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-01 17:50:19 +0100

    Remove unecessary clones

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,29 +1,33 @@
-use std::{env::{self}, fs::File, io::{BufRead, BufReader}};
+use std::{
+    env::{self},
+    fs::File,
+    io::{BufRead, BufReader},
+};
 fn main() {
-    let input = get_input(); 
-    println!("Part 1: {}", part_1(input.clone()));
-    println!("Part 2: {}", part_2(input));
+    let mut input = get_input();
+    println!("Part 1: {}", part_1(&mut input));
+    println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(mut locations : (Vec<i32>, Vec<i32>)) -> i32 {
+fn part_1(locations: &mut (Vec<i32>, Vec<i32>)) -> i32 {
     let mut output = 0;
-    
+
     locations.0.sort_unstable();
     locations.1.sort_unstable();
-    
-    for i in 0..= locations.0.len() - 1 {
+
+    for i in 0..=locations.0.len() - 1 {
         output += (locations.0[i] - locations.1[i]).abs();
     }
 
     return output;
 }
 
-fn part_2(locations : (Vec<i32>, Vec<i32>)) -> i32 {
+fn part_2(locations: &(Vec<i32>, Vec<i32>)) -> i32 {
     let mut output = 0;
-    
-    for i in locations.0 {
+
+    for i in &locations.0 {
         let mut count = 0;
-        for j in locations.1.clone() {
+        for j in &locations.1 {
             if i == j {
                 count += 1;
             }
@@ -38,12 +42,12 @@ fn get_input() -> (Vec<i32>, Vec<i32>) {
     let args: Vec<String> = env::args().collect();
     let input_path = format!("src/{}", &args[1]);
     let file = File::open(input_path).expect("Failed to open file");
-    let reader = BufReader::new(file);
+    let lines = BufReader::new(file).lines();
 
-    let mut left= Vec::new();
+    let mut left = Vec::new();
     let mut right = Vec::new();
 
-    for line_res in reader.lines()  {
+    for line_res in lines {
         let line = line_res.expect("");
         let numbers = line
             .split_whitespace()
