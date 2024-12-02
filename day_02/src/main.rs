c9bed63987692b771f6fc3b98f271da61d7345c9
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-02 07:01:14 +0100

    Day 2, part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,51 +1,66 @@
-use core::num;
 use std::{
     env::{self},
     fs::File,
-    io::{BufRead, BufReader}, process::Output,
+    io::{BufRead, BufReader},
 };
+
 fn main() {
-    let mut input = get_input();
-    println!("Part 1: {}", part_1(&mut input));
-    // println!("Part 2: {}", part_2(&input));
+    let input = get_input();
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(locations: &mut Vec<Vec<i32>>) -> i32 {
-    let mut output = 0;
+fn part_1(reports: &Vec<Vec<i32>>) -> i32 {
+    let mut safe: i32 = 0;
 
-    for report in locations {
-        let increase = report[0] < report[1];
-        let mut is_safe = true;
-
-        for i in 0..=report.len() - 2   {
-            let current = report[i];
-            let next = report[i + 1];
-            let diff = (current - next).abs();
-
-            println!("{:?}, curr: {}, next: {}", report, current, next);
-
-            if ((current < next) != increase) 
-                || diff < 1
-                || diff > 3  {
-                is_safe = false;
-                println!("breaking");
-                break;
-            }
-        }
-
-        if is_safe {
-            output += 1;
+    for report in reports {
+        if is_safe(report) {
+            safe += 1;
         }
     }
 
-    return output;
+    return safe;
 }
 
-// fn part_2(locations: &mut Vec<Vec<i32>>) -> i32 {
-//     let mut output = 0;
+fn part_2(reports: &Vec<Vec<i32>>) -> i32 {
+    let mut safe: i32 = 0;
 
-//     return output;
-// }
+    for report in reports {
+        if is_safe(report) {
+            safe += 1;
+        } else {
+            for i in 0..=report.len() - 1  {
+                let mut without_one = report.clone();
+                without_one.remove(i);
+
+                if is_safe(&without_one) {
+                    safe += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    return safe;
+}
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    let increase = report[0] < report[1];
+
+    for i in 0..=report.len() - 2 {
+        let current = report[i];
+        let next = report[i + 1];
+        let diff = (current - next).abs();
+
+        if (current < next) != increase
+            || diff < 1 
+            || diff > 3 {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 fn get_input() -> Vec<Vec<i32>> {
     let args: Vec<String> = env::args().collect();
@@ -61,8 +76,9 @@ fn get_input() -> Vec<Vec<i32>> {
             .split_whitespace()
             .map(|x| x.parse::<i32>().expect("parse error"))
             .collect::<Vec<i32>>();
+
         output.push(numbers);
-        }
+    }
 
     return output;
 }
