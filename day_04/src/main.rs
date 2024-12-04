2af97a4a0c85ded85860267d714dd5497f1becc9
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-04 06:18:08 +0100

    Small code improvements

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -18,73 +18,32 @@ fn part_1(input: &Input) -> i64 {
     let y_len = input.len();
     let x_len = input[0].len();
 
-    for y in 0..y_len {
-        for x in 0..x_len {
-            if x + 3 < x_len {
-                output += check_xmas(
-                    input[y][x],
-                    input[y][x + 1],
-                    input[y][x + 2],
-                    input[y][x + 3],
-                )
-            }
-            if (x as i64) - 3 >= 0 {
-                output += check_xmas(
-                    input[y][x],
-                    input[y][x - 1],
-                    input[y][x - 2],
-                    input[y][x - 3],
-                )
-            }
-            if y + 3 < y_len {
-                output += check_xmas(
-                    input[y][x],
-                    input[y + 1][x],
-                    input[y + 2][x],
-                    input[y + 3][x],
-                )
-            }
-            if (y as i64) - 3 >= 0 {
-                output += check_xmas(
-                    input[y][x],
-                    input[y - 1][x],
-                    input[y - 2][x],
-                    input[y - 3][x],
-                )
-            }
-
-            if x + 3 < x_len && y + 3 < y_len {
-                output += check_xmas(
-                    input[y][x],
-                    input[y + 1][x + 1],
-                    input[y + 2][x + 2],
-                    input[y + 3][x + 3],
-                )
-            }
-            if x + 3 < x_len && (y as i64) - 3 >= 0 {
-                output += check_xmas(
-                    input[y][x],
-                    input[y - 1][x + 1],
-                    input[y - 2][x + 2],
-                    input[y - 3][x + 3],
-                )
-            }
-            if (x as i64) - 3 >= 0 && y + 3 < y_len {
-                output += check_xmas(
-                    input[y][x],
-                    input[y + 1][x - 1],
-                    input[y + 2][x - 2],
-                    input[y + 3][x - 3],
-                )
-            }
-            if (x as i64) - 3 >= 0 && (y as i64) - 3 >= 0 {
-                output += check_xmas(
-                    input[y][x],
-                    input[y - 1][x - 1],
-                    input[y - 2][x - 2],
-                    input[y - 3][x - 3],
-                )
-            }
+    for y in 0..y_len - 3 {
+        for x in 0..x_len - 3 {
+            output += check_xmas(
+                input[y][x],
+                input[y][x + 1],
+                input[y][x + 2],
+                input[y][x + 3],
+            );
+            output += check_xmas(
+                input[y][x],
+                input[y + 1][x],
+                input[y + 2][x],
+                input[y + 3][x],
+            );
+            output += check_xmas(
+                input[y][x],
+                input[y + 1][x + 1],
+                input[y + 2][x + 2],
+                input[y + 3][x + 3],
+            );
+            output += check_xmas(
+                input[y + 3][x],
+                input[y + 2][x + 1],
+                input[y + 1][x + 2],
+                input[y][x + 3],
+            );
         }
     }
 
@@ -97,38 +56,15 @@ fn part_2(input: &Input) -> i64 {
     let y_len = input.len();
     let x_len = input[0].len();
 
-    for y in 0..y_len {
-        for x in 0..x_len {
-            if y + 2 < y_len && x + 2 < x_len {
-                output += check_x_mas(
-                    input[y][x],
-                    input[y + 1][x + 1],
-                    input[y + 2][x + 2],
-                    input[y][x + 2],
-                    input[y + 2][x],
-                );
-                output += check_x_mas(
-                    input[y + 2][x + 2],
-                    input[y + 1][x + 1],
-                    input[y][x],
-                    input[y][x + 2],
-                    input[y + 2][x],
-                );
-                output += check_x_mas(
-                    input[y][x],
-                    input[y + 1][x + 1],
-                    input[y + 2][x + 2],
-                    input[y + 2][x],
-                    input[y][x + 2],
-                );
-                output += check_x_mas(
-                    input[y + 2][x + 2],
-                    input[y + 1][x + 1],
-                    input[y][x],
-                    input[y + 2][x],
-                    input[y][x + 2],
-                );
-            }
+    for y in 0..y_len - 2 {
+        for x in 0..x_len - 2 {
+            output += check_x_mas(
+                input[y][x],
+                input[y + 1][x + 1],
+                input[y + 2][x + 2],
+                input[y][x + 2],
+                input[y + 2][x],
+            );
         }
     }
 
@@ -136,14 +72,18 @@ fn part_2(input: &Input) -> i64 {
 }
 
 fn check_xmas(i_0: char, i_1: char, i_2: char, i_3: char) -> i64 {
-    if i_0 == 'X' && i_1 == 'M' && i_2 == 'A' && i_3 == 'S' {
+    let s = &(format!("{}{}{}{}", i_0, i_1, i_2, i_3));
+    if s == "XMAS" || s == "SAMX" {
         return 1;
     }
     return 0;
 }
 
 fn check_x_mas(i_0: char, i_1: char, i_2: char, i_3: char, i_4: char) -> i64 {
-    if i_0 == 'M' && i_1 == 'A' && i_2 == 'S' && i_3 == 'M' && i_4 == 'S' {
+    let s_1 = &(format!("{}{}{}", i_0, i_1, i_2));
+    let s_2 = &(format!("{}{}{}", i_3, i_1, i_4));
+
+    if (s_1 == "MAS" || s_1 == "SAM") && (s_2 == "MAS" || s_2 == "SAM") {
         return 1;
     }
     return 0;
