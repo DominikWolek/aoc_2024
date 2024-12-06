f16780a5dcc93e38c623db5d34264a14c0ff87a2
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-06 06:31:44 +0100

    Day 6, both parts

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-type Input = Vec<Vec<i64>>;
+type Input = Vec<Vec<char>>;
 
 fn main() {
     let input = get_input();
@@ -12,18 +12,135 @@ fn main() {
     println!("Part 2: {}", part_2(&input));
 }
 
+pub enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
 fn part_1(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+    return walk_map(input).expect("");
+}
 
-    for i in input {}
+fn walk_map(input: &Input) -> Option<i64> {
+    let mut output: i64 = 1;
+    let mut cnt: i64 = 1;
 
-    return output;
+    let y_len = input.len();
+    let x_len = input[0].len();
+
+    let mut x_pos = 0;
+    let mut y_pos = 0;
+    let mut dir = Direction::Up;
+
+    for y in 0..y_len {
+        for x in 0..x_len {
+            if input[y][x] == '^' {
+                x_pos = x;
+                y_pos = y;
+            }
+        }
+    }
+
+    let mut escape = false;
+    let mut new_input = input.clone();
+    new_input[y_pos][x_pos] = 'X';
+
+    while !escape {
+        if cnt as usize > x_len * y_len * 4 {
+            return None;
+        }
+        cnt += 1;
+        match dir {
+            Direction::Up => {
+                if new_input[y_pos - 1][x_pos] == '#' {
+                    dir = Direction::Right;
+                } else {
+                    y_pos -= 1;
+                    if new_input[y_pos][x_pos] == '.' {
+                        output += 1;
+                        new_input[y_pos][x_pos] = 'X';
+                    }
+                    if y_pos == 0 {
+                        escape = true;
+                    }
+                }
+            }
+            Direction::Down => {
+                if new_input[y_pos + 1][x_pos] == '#' {
+                    dir = Direction::Left;
+                } else {
+                    y_pos += 1;
+                    if new_input[y_pos][x_pos] == '.' {
+                        output += 1;
+                        new_input[y_pos][x_pos] = 'X';
+                    }
+                    if y_pos == y_len - 1 {
+                        escape = true;
+                    }
+                }
+            }
+            Direction::Right => {
+                if new_input[y_pos][x_pos + 1] == '#' {
+                    dir = Direction::Down;
+                } else {
+                    x_pos += 1;
+                    if new_input[y_pos][x_pos] == '.' {
+                        output += 1;
+                        new_input[y_pos][x_pos] = 'X';
+                    }
+                    if x_pos == x_len - 1 {
+                        escape = true;
+                    }
+                }
+            }
+            Direction::Left => {
+                if new_input[y_pos][x_pos - 1] == '#' {
+                    dir = Direction::Up;
+                } else {
+                    x_pos -= 1;
+                    if new_input[y_pos][x_pos] == '.' {
+                        output += 1;
+                        new_input[y_pos][x_pos] = 'X';
+                    }
+                    if x_pos == 0 {
+                        escape = true;
+                    }
+                }
+            }
+        }
+        // for line in new_input.clone() {
+        //     for c in line {
+        //         print!("{}", c);
+        //     }
+        //     println!("");
+        // }
+        // println!("")
+    }
+
+    return Some(output);
 }
 
 fn part_2(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+    let mut output = 0;
 
-    for i in input {}
+    let y_len = input.len();
+    let x_len = input[0].len();
+
+    for x in 0..x_len {
+        for y in 0..y_len {
+            println!("{}, {}", x, y);
+            if input[y][x] == '.' {
+                let mut with_obstacle = input.clone();
+                with_obstacle[y][x] = '#';
+
+                if walk_map(&with_obstacle).is_none() {
+                    output += 1;
+                }
+            }
+        }
+    }
 
     return output;
 }
@@ -38,12 +155,7 @@ fn get_input() -> Input {
 
     for line_res in lines {
         let line = line_res.expect("");
-        let numbers = line
-            .split_whitespace()
-            .map(|x| x.parse::<i64>().expect("parse error"))
-            .collect::<Vec<i64>>();
-
-        output.push(numbers);
+        output.push(line.chars().collect());
     }
 
     return output;
