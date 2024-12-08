4ba27105c23132e51e6bfe91bbb36baa3149f5e7
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-08 09:21:10 +0100

    Day 8, part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -10,126 +10,132 @@ use std::collections::HashSet;
 type Input = Vec<Vec<char>>;
 type Location = (i64, i64);
 type Antena = (char, Location);
-type Line = (char, Vec<Location>);
+type Pair = (Location, Location);
+type Line = (char, Pair);
 type Frequencies = HashMap<char, Vec<Location>>;
 
 fn main() {
     let input = get_input();
     println!("Part 1: {}", part_1(&input));
-    // println!("Part 2: {}", part_2(&input));
-}
-
-fn part_2(input: &Input) -> i64 {
-    let mut output: i64 = 0;
-
-    return output;
+    println!("Part 2: {}", part_2(&input));
 }
 
 fn part_1(input: &Input) -> usize {
-    let antenas = get_antenas(input);
-    // println!("antenas: {:?}", antenas);
-
-    let freqs = get_frequencies(&antenas);
-    // println!("freqs: {:?}", freqs);
-    let lines = get_lines(&freqs);
-    // println!("lines: {:?}", lines);
-
-    let anti = get_antinodes(&lines, input);
-    // println!("anti: {:?}", anti);
-
-    // for y in 0..input.len() {
-    //     for x in 0..input.len() {
-    //         if anti.contains(&(x as i64, y as i64)) {
-    //             print!("#")
-    //         } else {
-    //             print!("{}", input[y][x])
-    //         }
-    //     }
-    //     println!();
-    // }
+    let lines = lines(input);
+    let anti = antinodes(&lines, input);
     return anti.len();
 }
 
-fn get_antinodes(lines: &Vec<Line>, input: &Input) -> HashSet<Location> {
+fn part_2(input: &Input) -> usize {
+    let lines = lines(input);
+    let anti = all_antinodes(&lines, input);
+    return anti.len();
+}
+
+fn antinodes(lines: &Vec<Line>, input: &Input) -> HashSet<Location> {
     let mut out = HashSet::new();
+
     for line in lines {
         let symbol = line.0;
-        let loc_a = line.1[0];
-        let loc_b = line.1[1];
+        let locs = antinodes_impl(line.1);
 
-        let diff_x = loc_a.0 - loc_b.0;
-        let diff_y = loc_a.1 - loc_b.1;
-
-        // println!(
-        //     "loc:{:?} diff_x: {:?}, diff y: {:?}",
-        //     line.1, diff_x, diff_y
-        // );
-
-        let loc_c = (loc_a.0 + diff_x, loc_a.1 + diff_y);
-        let loc_d = (loc_b.0 - diff_x, loc_b.1 - diff_y);
-
-        // println!(
-        //     "loc:{:?} new_loc:{:?}, new_loc_2:{:?}",
-        //     line.1, loc_c, loc_d
-        // );
-
-        if check(loc_c, symbol, input) {
-            out.insert(loc_c);
-        }
-
-        if check(loc_d, symbol, input) {
-            out.insert(loc_d);
+        for loc in locs {
+            if check(loc, symbol, input) {
+                out.insert(loc);
+            }
         }
     }
     return out;
+}
+
+fn all_antinodes(lines: &Vec<Line>, input: &Input) -> HashSet<Location> {
+    let mut out = HashSet::new();
+    for line in lines {
+        let locs = all_antinodes_impl(line.1, input);
+        for loc in locs {
+            out.insert(loc);
+        }
+    }
+    return out;
+}
+
+fn all_antinodes_impl((loc_a, loc_b): Pair, input: &Input) -> HashSet<Location> {
+    let diff_x = loc_a.0 - loc_b.0;
+    let diff_y = loc_a.1 - loc_b.1;
+
+    let mut out = HashSet::new();
+    let mut cur: (i64, i64) = loc_a;
+    while check_bounds(cur, input) {
+        out.insert(cur);
+        cur = (cur.0 + diff_x, cur.1 + diff_y);
+    }
+
+    cur = loc_a;
+    while check_bounds(cur, input) {
+        out.insert(cur);
+        cur = (cur.0 - diff_x, cur.1 - diff_y);
+    }
+
+    return out;
+}
+
+fn check_bounds(location: Location, input: &Input) -> bool {
+    let y = location.1;
+    if y < 0 || y >= input.len() as i64 {
+        return false;
+    }
+
+    let x = location.0;
+    if x < 0 || x >= input[0].len() as i64 {
+        return false;
+    }
+    return true;
+}
+
+fn antinodes_impl((loc_a, loc_b): Pair) -> Vec<Location> {
+    let diff_x = loc_a.0 - loc_b.0;
+    let diff_y = loc_a.1 - loc_b.1;
+
+    let loc_c = (loc_a.0 + diff_x, loc_a.1 + diff_y);
+    let loc_d = (loc_b.0 - diff_x, loc_b.1 - diff_y);
+
+    vec![loc_c, loc_d]
 }
 
 fn check(location: Location, symbol: char, input: &Input) -> bool {
     let y = location.1 as usize;
-    if y < 0 || y >= input.len() {
-        return false;
-    }
-
     let x = location.0 as usize;
-    if x < 0 || x >= input[0].len() {
-        return false;
-    }
-    if input[y][x] == symbol {
-        return false;
-    }
-
-    return true;
+    return check_bounds(location, input) && !(input[y][x] == symbol);
 }
 
-fn get_lines(freqs: &Frequencies) -> Vec<Line> {
-    let naive_pairs: Vec<(&char, Vec<Vec<Location>>)> = freqs
-        .iter()
-        .map(|(symbol, locations)| (symbol, into_pair(&locations)))
-        .collect();
+fn lines(input: &Input) -> Vec<Line> {
+    let antenas = antenas(input);
+    let freqs = frequencies(&antenas);
 
-    naive_pairs
+    freqs
         .iter()
+        .map(|(symbol, locations)| (*symbol, into_pairs(&locations)))
         .map(|(symbol, vec_of_pairs)| {
-            let out: Vec<(&char, &Vec<Location>)> =
-                vec_of_pairs.iter().map(|pairs| (*symbol, pairs)).collect();
-            return out;
+            vec_of_pairs
+                .iter()
+                .map(|pairs| (symbol, *pairs))
+                .collect::<Vec<Line>>()
         })
         .flatten()
-        .map(|(symbol, rest)| (*symbol, rest.clone()))
         .collect()
 }
 
-fn into_pair(locations: &Vec<Location>) -> Vec<Vec<Location>> {
+fn into_pairs(locations: &Vec<Location>) -> Vec<Pair> {
     let mut out = Vec::new();
     for i in 0..locations.len() {
         for j in i + 1..locations.len() {
-            out.push(Vec::from([locations[i], locations[j]]));
+            out.push((locations[i], locations[j]));
         }
     }
     return out;
 }
 
-fn get_antenas(input: &Input) -> Vec<Antena> {
+fn antenas(input: &Input) -> Vec<Antena> {
     let mut out = Vec::new();
 
     for y in 0..input.len() {
@@ -143,20 +149,18 @@ fn get_antenas(input: &Input) -> Vec<Antena> {
     return out;
 }
 
-fn get_frequencies(antenas: &Vec<Antena>) -> Frequencies {
+fn frequencies(antenas: &Vec<Antena>) -> Frequencies {
     let mut out: Frequencies = HashMap::new();
-    for antena in antenas {
-        let v = out.get(&antena.0);
-        match v {
-            Some(v) => {
-                let mut c = v.clone();
-                c.push(antena.1);
-                out.insert(antena.0, c);
+    for (symbol, location) in antenas {
+        let current = out.get(&symbol);
+        match current {
+            Some(current) => {
+                let mut current = current.clone();
+                current.push(*location);
+                out.insert(*symbol, current);
             }
             None => {
-                let mut v = Vec::new();
-                v.push(antena.1);
-                out.insert(antena.0, v);
+                out.insert(*symbol, vec![*location]);
             }
         }
     }
