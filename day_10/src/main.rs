8ebfd93a8c1ea0b9e1858b7952d3b18698026abb
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-10 07:12:36 +0100

    Day 10, both parts

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     env::{self},
     fs::File,
     io::{BufRead, BufReader},
@@ -12,20 +13,69 @@ fn main() {
     println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+fn part_1(input: &Input) -> usize {
+    let mut output = 0;
 
-    for i in input {}
+    for y in 0..input.len() {
+        for x in 0..input[0].len() {
+            if input[y][x] == 0 {
+                let mut set: HashSet<(usize, usize)> = HashSet::new();
+                for val in calc_value(&input, (x, y)) {
+                    set.insert(val);
+                }
+                output += set.len();
+            }
+        }
+    }
 
     return output;
 }
 
-fn part_2(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+fn part_2(input: &Input) -> usize {
+    let mut output = 0;
 
-    for i in input {}
+    for y in 0..input.len() {
+        for x in 0..input[0].len() {
+            if input[y][x] == 0 {
+                output += calc_value(&input, (x, y)).len();
+            }
+        }
+    }
 
     return output;
+}
+
+fn calc_value(input: &Input, pos: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut stack = vec![(0, pos)];
+
+    let mut last_vals: Vec<(usize, usize)> = Vec::new();
+
+    while !stack.is_empty() {
+        let (curr_height, (x, y)) = stack.pop().expect("");
+
+        if curr_height == 9 {
+            last_vals.push((x, y));
+        } else {
+            let x = x as i64;
+            let y = y as i64;
+
+            let possible = vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)];
+
+            for (new_x, new_y) in possible {
+                if in_bounds(new_x, new_y, input.len(), input[0].len()) {
+                    let (new_x, new_y) = (new_x as usize, new_y as usize);
+                    if input[new_y][new_x] == curr_height + 1 {
+                        stack.push((curr_height + 1, (new_x, new_y)));
+                    }
+                }
+            }
+        }
+    }
+    return last_vals;
+}
+
+fn in_bounds(new_x: i64, new_y: i64, y_size: usize, x_size: usize) -> bool {
+    new_x >= 0 && new_y >= 0 && new_y < y_size as i64 && new_x < x_size as i64
 }
 
 fn get_input() -> Input {
@@ -39,8 +89,16 @@ fn get_input() -> Input {
     for line_res in lines {
         let line = line_res.expect("");
         let numbers = line
-            .split_whitespace()
-            .map(|x| x.parse::<i64>().expect("parse error"))
+            .chars()
+            .collect::<Vec<char>>()
+            .iter()
+            .map(|x| {
+                if *x == '.' {
+                    -1
+                } else {
+                    format!("{}", x).parse::<i64>().expect("parse error")
+                }
+            })
             .collect::<Vec<i64>>();
 
         output.push(numbers);
