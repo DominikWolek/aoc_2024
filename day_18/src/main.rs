a6420d9423397e15dcaec7dfd34e914558956edb
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-18 17:04:37 +0100

    Day 18, part 2

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -2,7 +2,6 @@ use std::{
     env::{self},
     fs::File,
     io::{BufRead, BufReader},
-    path,
 };
 
 use pathfinding::{grid::Grid, prelude::dijkstra};
@@ -13,42 +12,58 @@ type Input = Vec<Position>;
 const SIZE: usize = 70 + 1;
 const FALLEN: usize = 1024;
 
-// const SIZE: usize = 6 + 1;
-// const FALLEN: usize = 12;
-
 const START: Position = (0, 0);
 const END: Position = (SIZE - 1, SIZE - 1);
 
 fn main() {
     let input = get_input();
     println!("Part 1: {}", part_1(&input));
-    // println!("Part 2: {}", part_2(&input));
+    println!("Part 2: {:?}", part_2(&input).unwrap());
 }
 
-fn part_1(input: &Input) -> i64 {
-    let mut grid = input.iter().take(FALLEN).map(|x| *x).collect::<Grid>();
+fn part_1(input: &Input) -> usize {
+    let mut grid: Grid = input.iter().take(FALLEN).map(|x| *x).collect::<Grid>();
 
     grid.invert();
-
     grid.resize(SIZE, SIZE);
-    let successors = |pos: &Position| {
-        grid.neighbours(*pos)
-            .iter()
-            .map(|neighbour| (*neighbour, 1))
-            .collect::<Vec<_>>()
-    };
-    match dijkstra(&START, successors, |pos| *pos == END) {
+
+    match dijkstra(&START, succs_fun(grid), is_end_fun()) {
         Some((_path, path_len)) => return path_len,
         None => return 0,
     }
 }
 
-fn part_2(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+fn part_2(input: &Input) -> Option<Position> {
+    for fallen_cnt in 1..=input.len() {
+        let grid = grid_from_input(input, fallen_cnt);
+        if dijkstra(&START, succs_fun(grid), is_end_fun()).is_none() {
+            return Some(input[fallen_cnt - 1]);
+        }
+    }
 
-    // for i in input {}
+    return None;
+}
 
-    return output;
+fn grid_from_input(input: &Vec<Position>, fallen: usize) -> Grid {
+    let mut grid: Grid = input.iter().take(fallen).map(|x| *x).collect::<Grid>();
+    grid.resize(SIZE, SIZE);
+    grid.invert();
+
+    return grid;
+}
+
+fn is_end_fun() -> impl FnMut(&(usize, usize)) -> bool {
+    |pos| *pos == END
+}
+
+fn succs_fun(grid: Grid) -> impl Fn(&Position) -> Vec<(Position, usize)> {
+    let successors = move |pos: &Position| {
+        grid.neighbours(*pos)
+            .iter()
+            .map(|neighbour| (*neighbour, 1))
+            .collect::<Vec<(Position, usize)>>()
+    };
+    successors
 }
 
 fn get_input() -> Input {
