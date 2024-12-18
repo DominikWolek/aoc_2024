237bb529090b863f83cee6dc33503d312eb98afe
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-18 16:42:16 +0100

    Day 18, part 1

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -2,28 +2,51 @@ use std::{
     env::{self},
     fs::File,
     io::{BufRead, BufReader},
+    path,
 };
 
-type Input = Vec<Vec<i64>>;
+use pathfinding::{grid::Grid, prelude::dijkstra};
+
+type Position = (usize, usize);
+type Input = Vec<Position>;
+
+const SIZE: usize = 70 + 1;
+const FALLEN: usize = 1024;
+
+// const SIZE: usize = 6 + 1;
+// const FALLEN: usize = 12;
+
+const START: Position = (0, 0);
+const END: Position = (SIZE - 1, SIZE - 1);
 
 fn main() {
     let input = get_input();
     println!("Part 1: {}", part_1(&input));
-    println!("Part 2: {}", part_2(&input));
+    // println!("Part 2: {}", part_2(&input));
 }
 
 fn part_1(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+    let mut grid = input.iter().take(FALLEN).map(|x| *x).collect::<Grid>();
 
-    for i in input {}
+    grid.invert();
 
-    return output;
+    grid.resize(SIZE, SIZE);
+    let successors = |pos: &Position| {
+        grid.neighbours(*pos)
+            .iter()
+            .map(|neighbour| (*neighbour, 1))
+            .collect::<Vec<_>>()
+    };
+    match dijkstra(&START, successors, |pos| *pos == END) {
+        Some((_path, path_len)) => return path_len,
+        None => return 0,
+    }
 }
 
 fn part_2(input: &Input) -> i64 {
     let mut output: i64 = 0;
 
-    for i in input {}
+    // for i in input {}
 
     return output;
 }
@@ -38,12 +61,9 @@ fn get_input() -> Input {
 
     for line_res in lines {
         let line = line_res.expect("");
-        let numbers = line
-            .split_whitespace()
-            .map(|x| x.parse::<i64>().expect("parse error"))
-            .collect::<Vec<i64>>();
+        let (x, y) = line.split_once(",").unwrap();
 
-        output.push(numbers);
+        output.push((x.parse().expect(""), y.parse().expect("")));
     }
 
     return output;
