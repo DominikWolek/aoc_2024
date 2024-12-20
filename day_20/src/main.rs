682dabac459a8de83961013c31ab08f6466f6b28
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-20 16:10:14 +0100

    Day 20, part 1

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -4,28 +4,118 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-type Input = Vec<Vec<i64>>;
+use pathfinding::{
+    grid::{self, Grid},
+    prelude::dijkstra,
+};
+
+type Position = (usize, usize);
+type Input = Vec<Vec<char>>;
 
 fn main() {
-    let input = get_input();
-    println!("Part 1: {}", part_1(&input));
-    println!("Part 2: {}", part_2(&input));
+    let (grid, start, end) = racetrack();
+
+    println!("Part 1: {}", part_1(&grid, &start, &end));
+    // println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(input: &Input) -> i64 {
+fn part_1(input: &Grid, start: &Position, end: &Position) -> i64 {
     let mut output: i64 = 0;
 
-    for i in input {}
+    let base_time = best_path(start, end, input).unwrap().1;
+
+    let mut inverted_grid = input.clone();
+    inverted_grid.invert();
+
+    let mut grid_with_cheats = input.clone();
+
+    for cheat_start in &inverted_grid {
+        grid_with_cheats.add_vertex(cheat_start);
+
+        if let Some(path_to_cheat) = best_path(start, &cheat_start, &grid_with_cheats) {
+            let neighbs = grid_with_cheats.neighbours(cheat_start);
+            let good_neighbours = neighbs
+                .iter()
+                .filter(|neighbour| !path_to_cheat.0.contains(neighbour))
+                .collect::<Vec<_>>();
+            for cheat_end in good_neighbours {
+                let time_to_end = best_path(&cheat_end, end, &grid_with_cheats).unwrap().1;
+
+                let cheat_time = base_time as i64 - (path_to_cheat.1 + time_to_end + 1) as i64;
+                if cheat_time >= 100 {
+                    // println!("{:?} {:?} {:?}", cheat_start, cheat_end, cheat_time)
+                    output += 1;
+                }
+            }
+        };
+        grid_with_cheats.remove_vertex(cheat_start);
+    }
 
     return output;
 }
 
-fn part_2(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+fn best_path(start: &Position, end: &Position, grid: &Grid) -> Option<(Vec<Position>, usize)> {
+    dijkstra(start, succs_fun(grid), |pos| *pos == *end)
+}
 
-    for i in input {}
+fn succs_fun(grid: &Grid) -> impl Fn(&Position) -> Vec<(Position, usize)> + use<'_> {
+    let successors = |pos: &Position| {
+        grid.neighbours(*pos)
+            .iter()
+            .map(|neighbour| (*neighbour, 1))
+            .collect::<Vec<(Position, usize)>>()
+    };
+    successors
+}
 
-    return output;
+fn find(input: &Input, arg: char) -> Option<Position> {
+    for y in 0..input.len() {
+        for x in 0..input[0].len() {
+            if input[y][x] == arg {
+                return Some((x, y));
+            }
+        }
+    }
+    return None;
+}
+
+// fn part_2(input: &Input) -> i64 {
+//     let mut output: i64 = 0;
+
+//     for i in input {}
+
+//     return output;
+// }
+
+fn racetrack() -> (Grid, Position, Position) {
+    let mut input: Vec<Vec<char>> = get_input();
+
+    let s_cord = find(&input, 'S').unwrap();
+    let e_cord = find(&input, 'E').unwrap();
+
+    input[s_cord.1][s_cord.0] = '.';
+    input[e_cord.1][e_cord.0] = '.';
+    let grid = grid_from_input(&input);
+
+    return (grid, s_cord, e_cord);
+}
+
+fn grid_from_input(input: &Input) -> Grid {
+    input
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            let enumerated = line
+                .iter()
+                .enumerate()
+                .map(|(x, c)| (x, y, *c))
+                .collect::<Vec<(usize, usize, char)>>();
+
+            enumerated
+        })
+        .flatten()
+        .filter_map(|(x, y, c)| (c == '.').then(|| (x, y)))
+        .collect::<Grid>()
 }
 
 fn get_input() -> Input {
@@ -38,12 +128,9 @@ fn get_input() -> Input {
 
     for line_res in lines {
         let line = line_res.expect("");
-        let numbers = line
-            .split_whitespace()
-            .map(|x| x.parse::<i64>().expect("parse error"))
-            .collect::<Vec<i64>>();
+        let characters = line.chars().collect::<Vec<_>>();
 
-        output.push(numbers);
+        output.push(characters);
     }
 
     return output;
