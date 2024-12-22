91401e3a91f0fd8323d77f6593a2612e7dc375c5
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-22 15:50:10 +0100

    Day 22, part 1

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-type Input = Vec<Vec<i64>>;
+type Input = Vec<usize>;
 
 fn main() {
     let input = get_input();
@@ -12,12 +12,14 @@ fn main() {
     println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(input: &Input) -> i64 {
-    let mut output: i64 = 0;
+const GENERATIONS_CNT: usize = 2000;
+const PRUNE_BASE: usize = 16_777_216;
 
-    for i in input {}
-
-    return output;
+fn part_1(input: &Input) -> usize {
+    input
+        .iter()
+        .map(|initial| calc_secret(*initial, GENERATIONS_CNT))
+        .sum()
 }
 
 fn part_2(input: &Input) -> i64 {
@@ -28,23 +30,38 @@ fn part_2(input: &Input) -> i64 {
     return output;
 }
 
+fn calc_secret(secret: usize, generations_cnt: usize) -> usize {
+    if generations_cnt == 0 {
+        return secret;
+    } else {
+        let mut current = mix(secret << 6, secret);
+        current = prune(current);
+
+        current = mix(current >> 5, current);
+        current = prune(current);
+
+        current = mix((current * 2048), current);
+        current = prune(current);
+
+        return calc_secret(current, generations_cnt - 1);
+    }
+}
+
+fn mix(left: usize, right: usize) -> usize {
+    left ^ right
+}
+
+fn prune(current: usize) -> usize {
+    current % PRUNE_BASE
+}
+
 fn get_input() -> Input {
     let args: Vec<String> = env::args().collect();
     let input_path = format!("src/{}", &args[1]);
     let file = File::open(input_path).expect("Failed to open file");
-    let lines = BufReader::new(file).lines();
 
-    let mut output = Vec::new();
-
-    for line_res in lines {
-        let line = line_res.expect("");
-        let numbers = line
-            .split_whitespace()
-            .map(|x| x.parse::<i64>().expect("parse error"))
-            .collect::<Vec<i64>>();
-
-        output.push(numbers);
-    }
-
-    return output;
+    BufReader::new(file)
+        .lines()
+        .map(|line| line.expect("").parse::<usize>().expect("parse error"))
+        .collect()
 }
