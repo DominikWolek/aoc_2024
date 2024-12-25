8fbe58cc4fbdc5f572d19f10e39808b46a3c3598
DominikWolek <dominik.artur.wolek@gmail.com>
2024-12-25 07:32:20 +0100

    Day 25, part 1
    Merry Christmas

--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -4,26 +4,30 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-type Input = Vec<Vec<i64>>;
+type Height = i8;
+type Key = Vec<Height>;
+type Lock = Vec<Height>;
+
+type Input = (Vec<Lock>, Vec<Key>);
+
+const GROUP_HEIGHT: usize = 7;
 
 fn main() {
     let input = get_input();
     println!("Part 1: {}", part_1(&input));
-    println!("Part 2: {}", part_2(&input));
+    println!("Merry Christmas!");
 }
 
-fn part_1(input: &Input) -> i64 {
+fn part_1((locks, keys): &Input) -> i64 {
     let mut output: i64 = 0;
 
-    for i in input {}
-
-    return output;
-}
-
-fn part_2(input: &Input) -> i64 {
-    let mut output: i64 = 0;
-
-    for i in input {}
+    for lock in locks {
+        for key in keys {
+            if (0..5).all(|i| lock[i] + key[i] <= 5) {
+                output += 1;
+            }
+        }
+    }
 
     return output;
 }
@@ -32,19 +36,57 @@ fn get_input() -> Input {
     let args: Vec<String> = env::args().collect();
     let input_path = format!("src/{}", &args[1]);
     let file = File::open(input_path).expect("Failed to open file");
-    let lines = BufReader::new(file).lines();
+    let lines: Vec<Vec<char>> = BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap().chars().collect())
+        .collect();
 
-    let mut output = Vec::new();
+    let mut groupped: Vec<Vec<Vec<char>>> = Vec::new();
+    let mut i = 0;
 
-    for line_res in lines {
-        let line = line_res.expect("");
-        let numbers = line
-            .split_whitespace()
-            .map(|x| x.parse::<i64>().expect("parse error"))
-            .collect::<Vec<i64>>();
+    while i < lines.len() {
+        let group = lines[i..i + GROUP_HEIGHT]
+            .iter()
+            .map(|x| x.clone())
+            .collect::<Vec<Vec<char>>>();
 
-        output.push(numbers);
+        groupped.push(group);
+        i += GROUP_HEIGHT + 1;
     }
 
-    return output;
+    let mut keys: Vec<Key> = Vec::new();
+    let mut locks: Vec<Lock> = Vec::new();
+
+    for group in groupped {
+        let mut key = false;
+        let mut vals: Vec<i8> = vec![-1, -1, -1, -1, -1];
+
+        if group[0] == vec!['.', '.', '.', '.', '.'] {
+            key = true;
+
+            for y in (0..GROUP_HEIGHT).rev() {
+                count_height(&group, &mut vals, y);
+            }
+        } else {
+            for y in 0..GROUP_HEIGHT {
+                count_height(&group, &mut vals, y);
+            }
+        }
+
+        if key {
+            keys.push(vals);
+        } else {
+            locks.push(vals);
+        }
+    }
+
+    (locks, keys)
+}
+
+fn count_height(group: &Vec<Vec<char>>, vals: &mut Vec<i8>, y: usize) {
+    for x in 0..5 {
+        if group[y][x] == '#' {
+            vals[x] += 1;
+        }
+    }
 }
